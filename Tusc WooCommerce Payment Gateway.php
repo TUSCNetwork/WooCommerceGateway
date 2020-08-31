503fdd60c6330533337f110bed1b70e73cd5f625
Kyle Sponable <kylesponable@gmail.com>
2020-08-31 17:33:42 -0400

    added get

--- Tusc WooCommerce Payment Gateway.php
+++ Tusc WooCommerce Payment Gateway.php
@@ -117,4 +117,42 @@ add_filter( 'woocommerce_payment_gateways', 'add_to_woo_tusc_payment_gateway');
 function add_to_woo_tusc_payment_gateway( $gateways ) {
     $gateways[] = 'WC_Tusc_pay_Gateway';
     return $gateways;
+
+}
+
+function CallAPI($method, $url, $data = false)
+{
+    $curl = curl_init();
+
+    switch ($method)
+    {
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
+
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+        case "PUT":
+            curl_setopt($curl, CURLOPT_PUT, 1);
+            break;
+        case "GET":
+            curl_setopt($curl, option: CURLOPT_HTTPGET, value: 1);
+
+        default:
+            if ($data)
+                $url = sprintf("%s?%s", $url, http_build_query($data));
+    }
+
+    // Optional Authentication:
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    $result = curl_exec($curl);
+
+    curl_close($curl);
+
+    return $result;
 }
